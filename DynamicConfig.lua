a04127e142c2141933a2b8db5f410b24b2335b7a
Rick Blommers <rick@blommersit.nl>
2014-05-05 19:34:14 +0200

    Import dynamic graphics configuration depending on combat mode

---
+++ DynamicConfig.lua
@@ -0,0 +1,90 @@
+﻿--[[==========================================
+	DynamicConfig
+	==========================================
+	Dynamicly downscale graphics quality when in combat. 
+	For a smooter combat response with a graphicly pleasing world while exploring.
+  ]]--
+
+DynamicConfig = {}
+DynamicConfig.name = "DynamicConfig"
+DynamicConfig.command = "/dynconf"
+DynamicConfig.versionString = "v1.0.0"
+DynamicConfig.versionSettings = 1;
+DynamicConfig.versionBuild = 0;
+
+
+--[[==========================================
+	Initialize
+	==========================================]]--
+function DynamicConfig.Initialize( eventCode, addOnName )
+
+	-- Only set up for DynamicConfig
+	if ( addOnName ~= DynamicConfig.name ) then
+		return
+	end
+	
+	EVENT_MANAGER:RegisterForEvent( DynamicConfig.name, EVENT_PLAYER_COMBAT_STATE, DynamicConfig.CombatStateEvent );	
+	SLASH_COMMANDS[DynamicConfig.command] = DynamicConfig.SlashCommands;
+end
+
+-- Hook initialization onto the ADD_ON_LOADED event
+EVENT_MANAGER:RegisterForEvent( DynamicConfig.name, EVENT_ADD_ON_LOADED, DynamicConfig.Initialize )
+
+--[[==========================================
+	Configure here your high quality exploring configuration
+	==========================================]]--
+
+function DynamicConfig.qualityUp()
+	SetCVar('SUB_SAMPLING',2)
+	SetCVar('AMBIENT_OCCLUSION',1)
+	SetCVar('ANTI_ALIASING_v2', 1)
+	SetCVar('BLOOM', 1)
+	d("quality UP")
+end
+
+
+--[[==========================================
+	Configure here your low quality combat configuration 
+	==========================================]]--
+
+function DynamicConfig.qualityDown()
+	SetCVar('SUB_SAMPLING',0)
+	SetCVar('AMBIENT_OCCLUSION',0)
+	SetCVar('ANTI_ALIASING_v2', 0)
+	SetCVar('BLOOM', 0)
+	d("quality down")
+end
+
+
+--[[==========================================
+
+	==========================================]]--
+function DynamicConfig.CombatStateEvent( eventCode, inCombat )
+	
+	if ( inCombat == true ) then
+		DynamicConfig.qualityDown()
+	else
+		DynamicConfig.qualityUp()
+	end
+
+end
+
+
+function DynamicConfig.SlashCommands( text )
+
+	if ( text == "" ) then
+		d( "DynamicConfig " .. DynamicConfig.versionString );
+		d( "Command line options: /dynconfig up down :);" );
+		return
+	end
+
+	
+	if ( text == "up" ) then
+		DynamicConfig.qualityUp()
+		return
+	end
+	if ( text == "down" ) then
+		DynamicConfig.qualityDown()
+		return
+	end
+end
---
+++ README.md
@@ -0,0 +1,52 @@
+## What is this about?
+
+This is a very simple plugin for The Elder Scrolls Online.
+
+It tweaks the graphics configuration depending on combat status.
+When in combat the graphics quality is turned down, so the game is more responsive.
+When not in combat the quality is turned higher.
+
+
+## Setup
+
+1.  Go to the ESO documents directory at ```C:\Users\YOURUSER\Documents\Elder Scrolls Online\VERSION\``` (replace VERSION with the client you're using)
+    Or use the ```/Users/YOURUSER/Documents/Elder Scrolls Online/VERSION/AddOns``` on the mac
+2.  If a folder called ``Addons`` doesn't exist, create it.
+3.  Copy the DynamicConfig.lua script and DynamicConfig.txt to this directory
+
+
+## Configuration
+
+At the moment configuration can be done in the DynamicConfig.lua file:
+
+The qualityUp function sets the configuration for my high quality experience. (Non-Combat)
+
+
+```
+function DynamicConfig.qualityUp()
+    SetCVar('SUB_SAMPLING',2)
+    SetCVar('AMBIENT_OCCLUSION',1)
+    SetCVar('ANTI_ALIASING_v2', 1)
+    SetCVar('BLOOM', 1)
+    d("quality UP")
+end
+```
+
+
+The quality down function sets the lower quality for the combat mode 
+
+```
+function DynamicConfig.qualityDown()
+    SetCVar('SUB_SAMPLING',0)
+    SetCVar('AMBIENT_OCCLUSION',0)
+    SetCVar('ANTI_ALIASING_v2', 0)
+    SetCVar('BLOOm', 0)
+    d("quality down")
+end
+```
+
+
+
+## Ideas
+
+Perhaps build in a UI feature so you can toggle the different combat mode settings
